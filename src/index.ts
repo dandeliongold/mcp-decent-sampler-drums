1ff6c8ce44535512f8b7350bfbd980d909aac820
dandeliongold <83384332+dandeliongold@users.noreply.github.com>
2025-02-09 17:51:58 -0700

    Use music-metadata instead of wav-file-info

--- src/index.ts
+++ src/index.ts
@@ -87,7 +87,7 @@ function generateGroupsXml(config: DrumKitConfig): string {
 const server = new Server(
   {
     name: "decent-sampler-drums",
-    version: "0.0.1",
+    version: "0.0.3",
   },
   {
     capabilities: {
